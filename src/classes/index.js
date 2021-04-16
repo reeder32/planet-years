1d5df98e8d87dcf6c82a972ee9fc1726df545633
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-16 13:48:34 -0700

    add jquery submit button and rsult div in index.html

---
+++ src/classes/index.js
@@ -0,0 +1,8 @@
+import Planet from "./planet";
+import Mars from "./mars";
+import Jupiter from "./jupiter";
+import Mercury from "./mercury";
+import Venus from "./venus";
+import Person from "./person";
+
+export { Planet, Mars, Jupiter, Mercury, Venus, Person };
--- src/main.js
+++ src/main.js
@@ -2,5 +2,36 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import { Person, Mercury, Mars, Jupiter, Venus } from "./classes/index.js";
 
-$.ready(() => {});
+$("#submit-button").on("click", (event) => {
+  event.preventDefault();
+  const personName = $("#name").val();
+  const personAge = parseInt($("#age").val());
+  const personGender = $("#gender-selector option:selected").val();
+  const planetValue = parseInt($("#planet-selector option:selected").val());
+  let planet;
+  switch (planetValue) {
+    case 0:
+      planet = new Jupiter();
+      break;
+    case 1:
+      planet = new Mars();
+      break;
+    case 2:
+      planet = new Mercury();
+      break;
+    case 3:
+      planet = new Venus();
+      break;
+    default:
+      console.log("planet value is incorrect", planetValue);
+  }
+  console.log(planetValue);
+  if (personName && personAge && personGender) {
+    let person = new Person(personName, personAge, personGender);
+    person.planet = planet;
+  } else {
+    alert("You need to fill out all of the form, please...");
+  }
+});
