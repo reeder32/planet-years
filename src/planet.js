9d272edbc9ca5eac73f16eab37c6a6e553ec4cd4
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-16 08:45:39 -0700

    add pass conversion to years test

--- src/planet.js
+++ src/planet.js
@@ -2,6 +2,8 @@ export default class Planet {
   constructor(conversionNumber) {
       this.conversionNumber = conversionNumber;
   }
-
+convertNumberToYears(number) {
+  return number * this.conversionNumber;
+}
   
 }
